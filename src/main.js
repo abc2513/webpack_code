dcbefce6ff73c0ecc10d0f5e16a4173cc3438246
Xiaoguai <xiaoguaien@126.com>
2023-05-04 17:42:31 +0800

    39-高级-TreeShaking-（按需打包）

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import "./less/index.less"
 import "./sass/index.sass"
 import "./sass/index.scss"
 import "./stylus/index.styl"
+import {mul} from "./js/math"
 
+console.log('mul', mul(1, 2))
 // var a = 1 // 此时打包会报错
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
